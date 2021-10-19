641954272d50ed3f0033a38e40c991cfffde32e3
Dagore <david.gomez.redondo.dev@gmail.com>
2021-10-19 14:47:44 +0200

    19/10/2021

---
+++ enclase/EjemploFunciones/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EjemploFunciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            
+            for(i = 2; i <= 1000; i++)
+            {
+                if(IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        private static bool IsPrime (int n)
+        {
+            int number = n, counter = 0;
+
+            for (int i = 2; i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
