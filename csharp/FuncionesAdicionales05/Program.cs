4d26638534f4ca20c49dc2b17ff1357fa3db6799
Dagore <david.gomez.redondo.dev@gmail.com>
2021-10-27 17:09:48 +0200

    Intentando el rombo

---
+++ csharp/FuncionesAdicionales05/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace FuncionesAdicionales05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Escribe la función trianguloEstrellitas. La función te pedirá un número que deberá ser como mínimo el número 3 y como máximo el 20. Si el número no es correcto, escribirá “ERROR”. La función escribirá por pantalla una pirámide a base de estrellitas como la del ejemplo. La función deberá funcionar para cualquier número entre 3 y 20 (o sea, nada de escribir las estrellitas a mano).
+
+            Ej.: Tamaño = 4
+            *
+            **
+            ***
+            **** 
+            */
+
+            TrianguloEstrellitas(TriangleSize());
+            TrianguloEstrellitas(TriangleSize());
+            TrianguloEstrellitas(TriangleSize());
+        }
+
+        static int TriangleSize ()
+        {
+            int number;
+
+            Console.Write("Introduce un número natural igual o mayor que 3: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number < 3)
+            {
+                Console.WriteLine("ERROR");
+                number = 0;
+            }
+
+            return number;
+        }
+        static void TrianguloEstrellitas (int number)
+        {
+            string acc = "";
+
+            for (int i = 0; i < number; i++)
+            {
+                acc += "*";
+                Console.WriteLine(acc);
+            }
+        }
+    }
+}
---
+++ csharp/FuncionesAdicionales06/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FuncionesAdicionales06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Escribe la función rombo que te escribe un rombo en pantalla a base de asteriscos (la pirámide del ejercicio 6 sería la cuarta parte del rombo). Opcional: Hacerlo con el número de bucles más pequeño posible (se puede hacer sin bucles incluso).
+            
+            Ej.: Para un valor 4
+               **
+              ****
+             ******
+            ********
+             ******
+              ****
+               ** 
+            */
+            Rombo(TriangleSize());
+            Rombo(TriangleSize());
+            Rombo(TriangleSize());
+        }
+        static int TriangleSize()
+        {
+            int number;
+
+            Console.Write("Introduce un número natural: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("ERROR");
+                number = 0;
+            }
+
+            return number;
+        }
+        static void TrianguloEstrellitas(int number)
+        {
+            string acc = "";
+
+            for (int i = 0; i < number; i++)
+            {
+                acc += "*";
+                Console.WriteLine(acc);
+            }
+        }
+        static void Rombo (int number)
+        {
+
+        }
+    }
+}
