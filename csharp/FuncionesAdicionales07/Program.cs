62d433d687cfd1687be12c56921ee1cb23699253
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2021-11-02 14:12:57 +0100

    FuncionesAdicionales07 Terminado

--- csharp/FuncionesAdicionales07/Program.cs
+++ csharp/FuncionesAdicionales07/Program.cs
@@ -19,11 +19,23 @@ namespace FuncionesAdicionales07
                 19 20 21 22 23 24 25
                 26 7 28 29 30
             */
-
+            Console.WriteLine("Octubre 2021");
             CalendarioGrafico(10, 2021);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enero 2021");
+            CalendarioGrafico(1,2021);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enero 2020");
+            CalendarioGrafico(1,2020);
         }
         static int DiasMes(int month)
         {
+            //Escribe una función “diasMes”, a la que le pasamos un mes (en formato numérico 1 = Enero, 2 = Febrero, etc.) y nos dice el número de días que tiene ese mes.
+
             int[] monthDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
             return monthDays[month - 1];
@@ -52,6 +64,7 @@ namespace FuncionesAdicionales07
         }
         static int DiasMes2(int month, int year)
         {
+            //Debido a que en los años bisiestos el mes de Febrero tiene 29 días en lugar de 28, necesitaremos también dar el año para conseguir el resultado correcto. Escribe la función “diasMes2” a la que le pasas un mes y un año(ambos enteros) y te dice cuántos días tiene ese mes, teniendo en cuenta si el año es bisiesto o no (NOTA: Podéis usar la función “Bisiesto” que hicimos con anterioridad).
             int days;
             if (IsLeapYear(year) && month == 2)
             {
@@ -66,6 +79,7 @@ namespace FuncionesAdicionales07
         }
         static int DiasTranscurridos(int day, int month, int year)
         {
+            // Escribe una función “diasTranscurridos” a la que le pasamos tres enteros, el día, el mes y el año, y nos dice cuántos días han pasado desde el inicio del año hasta ese día. Así, el 1 de enero será el día 1 y el 31 de diciembre será el 365 ó 366 dependiendo de si el año es bisiesto o no.
             int acc = 0;
 
             for (int i = 1; i < month; i++)
@@ -77,45 +91,89 @@ namespace FuncionesAdicionales07
         }
         static int DiasTranscurridos1980(int day, int month, int year)
         {
+            //Escribe una función “diasTranscurridos1980” a la que le pasamos un día, mes y año y nos diga cuántos días han transcurrido desde el 01 / 01 / 1980 hasta ese día.
             int acc = 0;
 
-            for (int i = 1980; i < year - 1; i++)
+            for (int i = 1980; i < year; i++)
             {
-                acc += DiasTranscurridos(31, 12, i);
+                if (IsLeapYear(i))
+                {
+                    acc += 366;
+                }
+                else
+                {
+                    acc += 365;
+                }
             }
 
             return acc + DiasTranscurridos(day, month, year);
         }
         static int DiaSemanaFecha(int day, int month, int year)
         {
+            //Teniendo en cuenta que el 01/01/1980 era martes, haz una función “diaSemanaFecha” que nos devuelva qué día de la semana corresponde a la fecha(día, mes y año) que le pasamos en 3 parámetros.Nos devolverá un entero que corresponderá al día de la semana(1 = lunes, 2 = martes, etc.).
             int weekDay = 2, numberOfDays = DiasTranscurridos1980(day, month, year);
 
-            for (int i = 0; i <= numberOfDays; i++)
-            {
-                weekDay++;
+            return weekDay + (numberOfDays % 7) - 1;//Sumamos lo que sobra de coger semanas enteras y se resta uno porque los días transcurridos cuantan el primer día y el actual.
+        }
+        static void CalendarioGrafico(int month, int year)
+        {
+            int startingWeekday = DiaSemanaFecha(1, month, year), lastWeekday;
 
-                if (weekDay > 7)
-                {
-                    weekDay = 1;
-                }
+            Console.WriteLine(" L  M  X  J  V  S  D");
+            
+            lastWeekday = FirstCalendarRow(startingWeekday);
+
+            EndCalendar(lastWeekday, month, year);
+        }
+        static int FirstCalendarRow(int startingWeekday)
+        {
+            int lastWeekDay = 7 - startingWeekday + 1;
+            int[] startingCols = { 0, 1, 4, 7, 10, 13, 16, 19 };
+            string whitespaces = new string(' ', startingCols[startingWeekday] - 2);
+
+            for (int i = 1; i <= lastWeekDay; i++)
+            {
+                whitespaces += $"  {i}";
             }
 
-            return weekDay;
-        }
-        static void CalendarioGrafico (int month, int year)
-        {
-            int rows = 5, cols = 20, monthDays = DiasMes2(month, year), startingWeekday = DiaSemanaFecha(1, month, year);
-            
-            Console.WriteLine(" L  M  X  J  V  S  D");
-            FirstCalendarRow(startingWeekday, cols);
-        }
-        static void FirstCalendarRow (int startingWeekday, int cols)
-        {
-            int[] startingCols = { 0, 1, 4, 7, 10, 13, 16, 19};
+            Console.WriteLine(whitespaces);
 
-            string whitespaces = new String(' ', startingCols[startingWeekday]);
+            return lastWeekDay;
+        }
+        static void EndCalendar (int lastWeekday, int month, int year)
+        {
+            int j = 0, rows = 5, monthDays = DiasMes2(month, year);
 
-            Console.WriteLine(whitespaces + "*");
+            for (int i = 1; i < rows; i++)
+            {
+                lastWeekday++;
+                Console.Write(DayContent(lastWeekday));
+
+                while (lastWeekday < monthDays && j < 6)
+                {
+                    lastWeekday++;
+                    Console.Write(" " + DayContent(lastWeekday));
+                    j++;
+                }
+
+                j = 0;
+                Console.WriteLine();
+            }
+        }
+        static string DayContent (int number)
+        {
+            string result = "";
+
+            if (number < 10)
+            {
+                result = " " + number;
+            }
+            else
+            {
+                result += number;
+            }
+
+            return result;
         }
     }
 }
