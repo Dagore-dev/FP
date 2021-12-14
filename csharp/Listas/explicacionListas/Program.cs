6e47bc226fdf50af5640cfa826e36d804161a256
Dagore-dev <david.gomez.redondo.dev@gmail.com>
2021-12-14 14:59:22 +0100

    Explicación listas

---
+++ csharp/Listas/explicacionListas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace explicacionListas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                LISTAS
+            
+                Estructura de datos lineal y dinámica. Es decir, su principal diferencia con los arrays es que no tienen un tamaño establecido. La listas pueden implementarse como una interfaz de arrays o se pueden implementar como listas enlazadas donde cada nodo contiene un valor y una referencia al siguiente nodo. Estas últimas se recorren mucho peor que los arrays, las operaciones de inserción y elimininación son más eficientes.
+            */
+
+            List<int> l = new List<int>();//List<tipoDeDato> = new List<tipoDeDato>()
+
+            EscribeList(l);
+            l.Add(5);
+            EscribeList(l);
+            l[0] = 10;
+            EscribeList(l);
+        }
+        static void EscribeList (List<int> l)
+        {
+            Console.Write("< ");
+
+            for (int i = 0; i < l.Count; i++)//Count es el número de elementos actual. COUNT ES DINÁMICO
+            {
+                Console.Write(l[i] +  " ");
+            }
+
+            Console.Write(">");
+            Console.WriteLine();
+        }
+    }
+}
