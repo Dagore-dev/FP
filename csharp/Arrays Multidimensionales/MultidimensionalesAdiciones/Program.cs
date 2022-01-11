f3319f9e5db27033d0d609eec1fc9ea0ffc50ac7
Dagore <david.gomez.redondo.dev@gmail.com>
2022-01-11 20:45:30 +0100

    CalculaDeterminante

--- csharp/Arrays Multidimensionales/MultidimensionalesAdiciones/Program.cs
+++ csharp/Arrays Multidimensionales/MultidimensionalesAdiciones/Program.cs
@@ -36,7 +36,15 @@ namespace MultidimensionalesAdiciones
                     break;
                 case 2:
                     {
+                        int[,] a1 = { { 6, 1, 1 }, { 4, -2, 5 }, { 2, 8, 7 } }, a2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
+                        EscribeArrayBi(a1);
+                        Console.WriteLine($"|a1| = {CalculaDeterminante(a1)}");
+
+                        Console.WriteLine();
+
+                        EscribeArrayBi(a2);
+                        Console.WriteLine($"|a2| = {CalculaDeterminante(a2)}");
                     }
                     break;
                 case 3:
@@ -71,6 +79,28 @@ namespace MultidimensionalesAdiciones
             Console.Write("Introduzca su opción: ");
         }
         #region utils
+        static void EscribeArrays(int[] a)
+        {
+            string result = "";
+
+            if (a.Length > 0)
+            {
+                result += "[";
+
+                for (int i = 0; i < a.Length - 1; i++)
+                {
+                    result += a[i] + ", ";
+                }
+
+                result += $"{a[a.Length - 1]}]";
+            }
+            else
+            {
+                result += "[]";
+            }
+
+            Console.WriteLine(result);
+        }
         static void EscribeArrayBi(int[,] a)
         {
             for (int i = 0; i < a.GetLength(0); i++)
@@ -113,19 +143,41 @@ namespace MultidimensionalesAdiciones
         }
         static int CalculaDeterminante (int[,] a)
         {
-            int size = a.GetLength(0);
-            if (size == 3 && a.GetLength(1) == 3)
+            //https://www.mathsisfun.com/algebra/matrix-determinant.html
+            int row = a.GetLength(0), col = a.GetLength(1), result = 0;
+            if (row == 3 && col == 3)
             {
-
+                result = a[0, 0] * determinant2x2From3x3(a, 0) - a[0, 1] * determinant2x2From3x3(a, 1) + a[0, 2] * determinant2x2From3x3(a, 2);
             }
             else
             {
                 Console.WriteLine("ERROR: No es una matriz 3x3.");
             }
+
+            return result;
         }
-        static int Diagonal (int a, int b)
+        static int determinant2x2From3x3 (int[,] aa, int excluded)
         {
-            int result = 0;
+            int[] a = partialMatrixFrom3x3(aa, excluded);
+
+            return a[0] * a[3] - a[1] * a[2];
+        }
+        static int[] partialMatrixFrom3x3 (int[,] a, int excluded)
+        {
+            int row = a.GetLength(0), col = a.GetLength(1), k = 0;
+            int[] result = new int[4];
+
+            for (int i = 1; i < row; i++)//Primera fila siempre excluida.
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (j != excluded)
+                    {
+                        result[k] = a[i, j];
+                        k++;
+                    }
+                }
+            }
 
             return result;
         }
