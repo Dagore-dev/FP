35dcb6f861ae1c3104d331b3b9d85f9211668981
Dagore <david.gomez.redondo.dev@gmail.com>
2022-01-25 13:48:24 +0100

    EntregaFicherosRegistros

---
+++ csharp/Ficheros/EntregaFicherosRegistros/Program.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace EntregaFicherosRegistros
+{
+    class Program
+    {
+        public class FichaAlumno
+        {
+            public string nombre;
+            public int edad;
+            public double calificacion;
+        }
+        static void Main(string[] args)
+        {
+            List<FichaAlumno> alumnos = new List<FichaAlumno>();
+            int option;
+
+            ShowMenu();
+
+            option = int.Parse(Console.ReadLine());
+
+            while (option > 0 && option < 9)
+            {
+                switch (option)
+                {
+                    case 1:
+                        {
+                            LeeAlumnoLista(alumnos);
+                        }
+                        break;
+                    case 2:
+                        {
+                            ImprimeListaAlumnos(alumnos);
+                        }
+                        break;
+                    case 3:
+                        {
+                            EscribeFicheroAlumnosBinario(alumnos, "registroalumnosbinario.bin");
+                        }
+                        break;
+                    case 4:
+                        {
+                            LeeFicheroAlumnosBinario(alumnos, "registroalumnosbinario.bin");
+                        }
+                        break;
+                    case 5:
+                        {
+                            EscribeFicheroAlumnosTexto(alumnos, "registroalumnostexto.txt");
+                        }
+                        break;
+                    case 6:
+                        {
+                            LeeFicheroAlumnosTexto(alumnos, "registroalumnostexto.txt");
+                        }
+                        break;
+                    case 7:
+                        {
+                            EscribeFicheroAlumnosCSV(alumnos, "registroalumnos.csv");
+                        }
+                        break;
+                    case 8:
+                        {
+                            LeeFicheroAlumnosCSV(alumnos, "registroalumnos.csv");
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.Write("Introduzca otra opción o 0 para terminar: ");
+                option = int.Parse(Console.ReadLine());
+            }
+        }
+        #region utils
+        static void ShowMenu()
+        {
+            Console.WriteLine("MENU:");
+
+            Console.WriteLine("1 - LeeAlumnoLista");
+            Console.WriteLine("2 - ImprimeListaAlumnos");
+            Console.WriteLine("3 - EscribeFicheroAlumnosBinario");
+            Console.WriteLine("4 - LeeFicheroAlumnosBinario");
+            Console.WriteLine("5 - EscribeFicheroAlumnosTexto");
+            Console.WriteLine("6 - LeeFicheroAlumnosTexto");
+            Console.WriteLine("7 - EscribeFicheroAlumnosCSV");
+            Console.WriteLine("8 - LeeFicheroAlumnosCSV");
+
+            Console.Write("Introduce una opción: ");
+        }
+        static void PrintLine(int tableWidth)
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+        static void PrintRow(int tableWidth, params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (string column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+        #endregion
+        static FichaAlumno CreateFichaAlumno (string nombre, int edad, double calificacion)
+        {
+            FichaAlumno alumno = new FichaAlumno();
+
+            alumno.nombre = nombre;
+            alumno.edad = edad;
+            alumno.calificacion = calificacion;
+
+            return alumno;
+        }
+        static void LeeAlumnoLista (List<FichaAlumno> l)
+        {
+            string nombre;
+            int edad;
+            double calificacion;
+
+            Console.WriteLine("Introduce la información del nuevo alumno:");
+            
+            Console.Write("Nombre del alumno: ");
+            nombre = Console.ReadLine();
+
+            Console.Write("Edad del alumno: ");
+            edad = int.Parse(Console.ReadLine());
+
+            Console.Write("Calificación del alumno: ");
+            calificacion = double.Parse(Console.ReadLine());
+
+            l.Add(CreateFichaAlumno(nombre, edad, calificacion));
+        }
+        static void ImprimeListaAlumnos (List<FichaAlumno> l)
+        {
+            FichaAlumno alumno;
+            int tableWidth = 100;
+
+            PrintLine(tableWidth);
+            PrintRow(tableWidth, "Nombre", "Edad", "Calificación");
+
+            for (int i = 0; i < l.Count; i++)
+            {
+                alumno = l[i];
+
+                PrintLine(tableWidth);
+                PrintRow(tableWidth, alumno.nombre, alumno.edad.ToString(), String.Format("{0:0.00}", alumno.calificacion));
+            }
+        }
+        static void EscribeFicheroAlumnosBinario (List<FichaAlumno> l, string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Create);
+            BinaryWriter bw = new BinaryWriter(fs);
+            FichaAlumno alumno;
+
+            bw.Write(l.Count);
+
+            for (int i = 0; i < l.Count; i++)
+            {
+                alumno = l[i];
+
+                bw.Write(alumno.nombre);
+                bw.Write(alumno.edad);
+                bw.Write(alumno.calificacion);
+            }
+
+            bw.Close();
+            fs.Close();
+        }
+        static void LeeFicheroAlumnosBinario (List<FichaAlumno> l, string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+            FichaAlumno alumno;
+            int length = br.ReadInt32();
+            l.Clear();
+
+            for (int i = 0; i < length; i++)
+            {
+                alumno = new FichaAlumno();
+
+                alumno.nombre = br.ReadString();
+                alumno.edad = br.ReadInt32();
+                alumno.calificacion = br.ReadDouble();
+
+                l.Add(alumno);
+            }
+
+            br.Close();
+            fs.Close();
+        }
+        static void EscribeFicheroAlumnosTexto (List<FichaAlumno> l, string filename)
+        {
+            StreamWriter sw = new StreamWriter(filename);
+            FichaAlumno alumno;
+
+            for (int i = 0; i < l.Count; i++)
+            {
+                alumno = l[i];
+
+                sw.WriteLine(alumno.nombre);
+                sw.WriteLine(alumno.edad);
+                sw.WriteLine(alumno.calificacion);
+            }
+            
+            sw.Close();
+        }
+        static void LeeFicheroAlumnosTexto (List<FichaAlumno> l, string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            FichaAlumno alumno;
+
+            while (!sr.EndOfStream)
+            {
+                alumno = new FichaAlumno();
+
+                alumno.nombre = sr.ReadLine();
+                alumno.edad = int.Parse(sr.ReadLine());
+                alumno.calificacion = double.Parse(sr.ReadLine());
+
+                l.Add(alumno);
+            }
+
+            sr.Close();
+        }
+        static void EscribeFicheroAlumnosCSV (List<FichaAlumno> l, string filename)
+        {
+            StreamWriter sw = new StreamWriter(filename);
+            FichaAlumno alumno;
+
+            for (int i = 0; i < l.Count; i++)
+            {
+                alumno = l[i];
+
+                sw.WriteLine($"{alumno.nombre};{alumno.edad};{alumno.calificacion}");
+            }
+
+            sw.Close();
+        }
+        static void LeeFicheroAlumnosCSV (List<FichaAlumno> l, string filename)
+        {
+            StreamReader sr = new StreamReader(filename);
+            FichaAlumno alumno;
+            string[] record;
+
+            while (!sr.EndOfStream)
+            {
+                alumno = new FichaAlumno();
+                record = sr.ReadLine().Split(";");
+
+                alumno.nombre = record[0];
+                alumno.edad = int.Parse(record[1]);
+                alumno.calificacion = double.Parse(record[2]);
+
+                l.Add(alumno);
+            }
+
+            sr.Close();
+        }
+    }
+}
---
+++ csharp/Ficheros/ExplicacionRegistros/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExplicacionRegistros
+{
+    class Program
+    {
+        struct Car
+        {
+            string carPlate;
+            string brand;
+            string model;
+            bool isElectric;
+            int kilometers;
+        }
+        public class Coche
+        {
+            public string matricula;
+            public string marca;
+            public string modelo;
+            public bool esElectrico;
+            public int numeroKilometros;
+        }
+        static void Main(string[] args)
+        {
+            /*
+            Cada cosa que se mete en una base de datos es un registro (tambien llamado tupla), una estructura de datos que contiene datos de distintos tipos. Habitualmente no guardan una gran cantidad de datos.
+            Ejemplo:
+
+            struct Coche
+            {
+                string matricula;
+                string marca;
+                string modelo;
+                bool esElectrico;
+                int numeroKilimetros;
+            }
+
+            Los struct en C# dan problemas porque son bastante arcaicos. Vamos a usar clases como si fueran registros para evitar problemas.
+             */
+            Coche c1 = new Coche();
+            Coche c2 = new Coche();
+            List<Coche> l = new List<Coche>();    
+            
+            c1.matricula = "8745BCD";
+            c1.marca = "Fiat";
+            c1.modelo = "500";
+            c1.esElectrico = false;
+            c1.numeroKilometros = 200000;
+
+            c2.matricula = "9879PPK";
+            c2.marca = "Rover";
+            c2.modelo = "400";
+            c2.esElectrico = false;
+            c2.numeroKilometros = 256787;
+
+            l.Add(c1);
+            l.Add(c2);
+
+            for (int i = 0; i < l.Count; i++)
+            {
+                Console.WriteLine($"{l[i].marca} {l[i].modelo}");
+            }
+        }
+    }
+}
