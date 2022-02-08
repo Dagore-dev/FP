5d4a488b3cb796a0708b375349b69fffbb18dc94
Dagore <david.gomez.redondo.dev@gmail.com>
2022-02-08 21:27:18 +0100

    Ej4 examen en una función

--- csharp/Programación Examen 3/Ej4/Program.cs
+++ csharp/Programación Examen 3/Ej4/Program.cs
@@ -36,95 +36,60 @@ namespace Ej4
                           { 0, 0, 0, 0, 0, 0, 0, 0 },
                           { 0, 0, 0, 0, 0, 0, 0, 0 } };
 
-            Console.WriteLine("Tablero [0,0]");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 0, 0));
-            Console.WriteLine();
-            Console.WriteLine("Tablero [1,3] (True)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 1, 3));
-            Console.WriteLine();
+            Console.WriteLine("Tablero [0,0] (True)");
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 0, 0) + "\n");
+            
+            Console.WriteLine("Tablero [1,3] (False)");
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 1, 3) + "\n");
+            
             Console.WriteLine("Tablero [1,5] (True)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 1, 5));
-            Console.WriteLine();
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 1, 5) + "\n");
+            
             Console.WriteLine("Tablero [1,4] (True)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 1, 4));
-            Console.WriteLine();
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 1, 4) + "\n");
+            
             Console.WriteLine("Tablero [6,2] (True)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 6, 2));
-            Console.WriteLine();
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 6, 2) + "\n");
+            
             Console.WriteLine("Tablero [7,0] (True)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 7, 0));
-            Console.WriteLine();
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 7, 0) + "\n");
+            
             Console.WriteLine("Tablero [3,2] (True)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 3, 2));
-            Console.WriteLine();
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 3, 2) + "\n");
+            
             Console.WriteLine("Tablero [3,1] (True)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 3, 1));
-            Console.WriteLine();
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 3, 1) + "\n");
+            
             Console.WriteLine("Tablero [4,6] (True)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 4, 6));
-            Console.WriteLine();
-            Console.WriteLine("Tablero ajedrez vacío (False siempre)");
-            Console.WriteLine(Comprueba1Dama(tableroAjedrezVacio, 0, 0));
-            Console.WriteLine();
-            Console.WriteLine($"Comprueba dama = {CompruebaDama(tableroAjedrez)}");
-            Console.WriteLine($"Comprueba dama = {CompruebaDama(tableroAjedrezMalo)}");
+            Console.WriteLine(Comprueba1Dama(tableroAjedrez, 4, 6) + "\n");
+            
+            Console.WriteLine($"Comprueba dama = {CompruebaDamas(tableroAjedrez)}");
+            Console.WriteLine($"Comprueba dama = {CompruebaDamas(tableroAjedrezVacio)}");
+            Console.WriteLine($"Comprueba dama = {CompruebaDamas(tableroAjedrezMalo)}");
         }
         static bool Comprueba1Dama (int[,] aa, int row, int col)
         {
-            //for (int i = 0; i < 8; i++)
-            //{
-            //    for (int j = 0; j < 8; j++)
-            //    {
-            //        if (i == row || j == col || (i - j) == (row - col) || (i + j) == row + col)
-            //        {
-            //            if (i != row || j != col && aa[i, j] != 0)
-            //            {
-            //                return true;
-            //            }
-            //        }
-            //    }
-            //}
-
-            //return false;
-            
             bool result = false;
 
-            if (CheckColUp(aa, row, col))
+            for (int i = 0; i < 8; i++)
             {
-                result = true;
-            }
-            if (CheckColDown(aa, row, col))
-            {
-                result = true;
-            }
-            if (CheckRowLeft(aa, row, col))
-            {
-                result = true;
-            }
-            if (CheckRowRight(aa, row, col))
-            {
-                result = true;
-            }
-            if (CheckDiagonalRightUp(aa, row, col))
-            {
-                result = true;
-            }
-            if (CheckDiagonalRightDown(aa, row, col))
-            {
-                result = true;
-            }
-            if (CheckDiagonalLeftDown(aa, row, col))
-            {
-                result = true;
-            }
-            if (CheckDiagonalLeftUp(aa, row, col))
-            {
-                result = true;
+                for (int j = 0; j < 8; j++)
+                {
+                    //Misma fila || Misma columna || Misma diagonal 0,0 1,1 2,2 || Misma diagonal 0,2 1,1 2,0
+                    if (i == row || j == col || (i - j) == (row - col) || (i + j) == row + col)
+                    {
+                        //Si no es la misma casilla que estamos comprobando y su valor es distinto de cero (hay un pieza)
+                        if ((i != row || j != col) && aa[i, j] != 0)
+                        {
+                            result = true;
+                        }
+                    }
+                }
             }
 
             return result;
         }
-        static bool CompruebaDama (int[,] aa)
+        static bool CompruebaDamas (int[,] aa)
         {
             bool result = false;
             int rows = aa.GetLength(0), cols = aa.GetLength(1);
@@ -142,144 +107,6 @@ namespace Ej4
                 }
             }
             
-            return result;
-        }
-        static bool CheckColUp (int[,] aa, int row, int col)
-        {
-            bool result = false;
-
-            if (row != 0)
-            {
-                for (int i = row - 1; i >= 0 ; i--)
-                {
-                    if (aa[i, col] != 0)
-                    {
-                        result = true;
-                    }
-                }
-            }
-
-            return result;
-        }
-        static bool CheckColDown (int[,] aa, int row, int col)
-        {
-            bool result = false;
-            int rows = aa.GetLength(0);
-
-            if (row + 1 != rows)
-            {
-                for (int i = row + 1; i < rows; i++)
-                {
-                    if (aa[i, col] != 0)
-                    {
-                        result = true;
-                    }
-                }
-            }
-
-            return result;
-        }
-        static bool CheckRowLeft (int[,] aa, int row, int col)
-        {
-            bool result = false;
-
-            if (col != 0)
-            {
-                for (int i = col - 1; i >= 0; i--)
-                {
-                    if (aa[row, i] != 0)
-                    {
-                        result = true;
-                    }
-                }
-            }
-
-            return result;
-        }
-        static bool CheckRowRight (int[,] aa, int row, int col)
-        {
-            bool result = false;
-            int cols = aa.GetLength(1);
-
-            if (col + 1 < cols)
-            {
-                for (int i = col + 1; i < cols; i++)
-                {
-                    if (aa[row, i] != 0)
-                    {
-                        result = true;
-                    }
-                }
-            }
-            
-            return result;
-        }
-        static bool CheckDiagonalRightUp (int[,] aa, int row, int col)
-        {
-            bool result = false;
-            int cols = aa.GetLength(1), i = row - 1, j = col + 1;
-
-            while (i >= 0 && j < cols)
-            {
-                if (aa[i, j] != 0)
-                {
-                    result = true;
-                }
-                i--;
-                j++;
-            }
-
-            return result;
-        }
-        static bool CheckDiagonalRightDown (int[,] aa, int row, int col)
-        {
-            bool result = false;
-            int rows = aa.GetLength(0), cols = aa.GetLength(1), i = row + 1, j = col + 1;
-
-            while (i < rows && j < cols)
-            {
-                if (aa[i,j] != 0)
-                {
-                    result = true;
-                }
-                i++;
-                j++;
-            }
-
-            return result;
-        }
-        static bool CheckDiagonalLeftDown (int[,] aa, int row, int col)
-        {
-            bool result = false;
-            int rows = aa.GetLength(0), i = row + 1, j = col - 1;
-
-            while (i < rows && j >= 0)
-            {
-                if (aa[i,j] != 0)
-                {
-                    result = true;
-                }
-                i++;
-                j--;
-            }
-
-            return result;
-        }
-        static bool CheckDiagonalLeftUp (int[,] aa, int row, int col)
-        {
-            bool result = false;
-            int i = row - 1, j = col - 1;
-
-            while (i > 0 && j > 0)
-            {
-                if (aa[i, j] != 0)
-                {
-                    result = true;
-                }
-                i--;
-                j--;
-            }
-
             return result;
         }
     }
